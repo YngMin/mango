901218dbc2ec71b2db71d640d3d83e0f951e01eb
YngMin <joseph@whitecube.co.kr>
2024-02-24 20:50:04 +0900

    chore: remove unused error

--- pkg/errors/query.go
+++ pkg/errors/query.go
@@ -3,6 +3,5 @@ package errors
 import "errors"
 
 var (
-	ErrNeedDatabase       = errors.New("need database")
-	ErrNeedCollectionName = errors.New("need collection name")
+	ErrNeedDatabase = errors.New("need database")
 )
