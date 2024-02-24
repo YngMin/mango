8494a74b74704c1c7570a1581c73844b0b9a8c19
YngMin <joseph@whitecube.co.kr>
2024-02-24 20:37:06 +0900

    feat: wrap update func

--- pkg/ymgo/collection.go
+++ pkg/ymgo/collection.go
@@ -3,6 +3,7 @@ package ymgo
 import (
 	"github.com/iancoleman/strcase"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"reflect"
 	"strings"
@@ -15,6 +16,8 @@ type Collection struct {
 type InsertOneResult *mongo.InsertOneResult
 
 type InsertManyResult *mongo.InsertManyResult
+
+type UpdateResult *mongo.UpdateResult
 
 func (c *Collection) InsertOne(ctx Context, document any) (result InsertOneResult, err error) {
 	result, err = c.collection.InsertOne(ctx, document)
@@ -56,6 +59,21 @@ func (c *Collection) Find(ctx Context, filter bson.M, dest any) (err error) {
 		return
 	}
 
+	return
+}
+
+func (c *Collection) UpdateOne(ctx Context, filter bson.M, update bson.M) (result UpdateResult, err error) {
+	result, err = c.collection.UpdateOne(ctx, filter, update)
+	return
+}
+
+func (c *Collection) UpdateMany(ctx Context, filter bson.M, update bson.M) (result UpdateResult, err error) {
+	result, err = c.collection.UpdateMany(ctx, filter, update)
+	return
+}
+
+func (c *Collection) UpdateByID(ctx Context, id primitive.ObjectID, update bson.M) (result UpdateResult, err error) {
+	result, err = c.collection.UpdateByID(ctx, id, update)
 	return
 }
 
